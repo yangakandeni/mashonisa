851586223574eb7375926a730d8b6b2bbdddf4bc
yangakandeni <kandeni.yanga@gmail.com>
2025-02-08 14:58:26 +0200

    feat: Add main script

---
+++ mashonisa.pl
@@ -0,0 +1,7 @@
+#!/user/bin/env perl
+
+use strict;
+use warnings;
+use feature qw/ say signatures /;
+
+# As a user I want to enter the loan details, such as amount, name, date borrowed, interest applied
