d344ec5b9a6e132f83b6f0a4536adb33748c739c
yangakandeni <kandeni.yanga@gmail.com>
2025-02-10 21:26:26 +0200

    Fix: Use search_rs instead of find for chained calls
    The `find` method returns `undef` when no results are found,
    which can break chained calls.  This commit replaces `find`
    with `search_rs`, which returns an empty ResultSet in such
    cases, allowing chained calls to continue without error.

--- lib/Mashonisa/Schema/ResultSet/Agent.pm
+++ lib/Mashonisa/Schema/ResultSet/Agent.pm
@@ -6,6 +6,6 @@ use warnings;
 use experimental qw/ signatures /;
 use base 'DBIx::Class::ResultSet';
 
-sub by_name ($self, $name) { return $self->find({ name => $name }) }
+sub by_name ($self, $name) { return $self->search_rs({ name => $name }) }
 
 1;
