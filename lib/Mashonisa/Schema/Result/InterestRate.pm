092f06a890aa81419a64fde688be80996f1ba9b7
yangakandeni <kandeni.yanga@gmail.com>
2025-02-13 20:07:04 +0200

    feat: Add an interest rate table.
    This will help agents set their interest rates "globally"

---
+++ lib/Mashonisa/Schema/Result/InterestRate.pm
@@ -0,0 +1,33 @@
+package Mashonisa::Schema::Result::InterestRate;
+
+use strict;
+use warnings;
+
+use base 'DBIx::Class::Core';
+
+__PACKAGE__->load_components('InflateColumn::DateTime', 'ResultSet::HashRef');
+
+__PACKAGE__->table('interest_rate');
+
+__PACKAGE__->add_columns(
+    id => {
+        data_type => 'integer',
+        is_auto_increment => 1,
+        is_nullable => 0,
+    },
+    amount => {
+        data_type => 'decimal',
+        is_nullable => 0,
+        is_numeric => 1,
+        size => [ 9, 2 ],
+    },
+);
+
+__PACKAGE__->set_primary_key('id');
+
+__PACKAGE__->belongs_to(
+    'agent' => 'Mashonisa::Schema::Result::Agent',
+    { 'foreign.interest_rate_id' => 'self.id' },
+);
+
+1;
