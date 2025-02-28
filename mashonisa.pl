fb73596a6d7a58ac1ddf87ff4a034190d4e0d459
yangakandeni <kandeni.yanga@gmail.com>
2025-02-28 02:15:37 +0200

    feat: Update script with agent CRUD logic.
    This change on the main script introduces the logic
    for creating, reading, updating and deleting agents.

--- mashonisa.pl
+++ mashonisa.pl
@@ -7,6 +7,7 @@ use experimental qw/ say signatures /;
 
 use lib 'lib';
 use Mashonisa;
+use Mashonisa::Model::Agent;
 
 run_app();
 
@@ -16,49 +17,161 @@ sub run_app {
     display_select_option_message();
     display_menu_options();
 
+    my $MashonisaAgent = Mashonisa::Model::Agent->new;
+
     while (1) {
         try {
+            # TODO: cater for when selection is not numeric.
+            my $selection = get_selected_option();
+            die "Invalid selection - ". $selection unless $selection && $selection > 0;
 
-        my $selection = get_selected_option();
-        if ($selection == 1) {
-            print "Enter agent name: ";
-            chomp(my $name = <STDIN>);
-            Mashonisa->add_agent($name);
-        }
-        # elsif ($selection == 2) { Mashonisa->view_agents(); }
-        # elsif ($selection == 3) { Mashonisa->update_agent(); }
-        # elsif ($selection == 4) { Mashonisa->delete_agent(); }
-        # elsif ($selection == 5) { Mashonisa->add_client(); }
-        # elsif ($selection == 6) { Mashonisa->view_clients(); }
-        # elsif ($selection == 7) { Mashonisa->update_client(); }
-        # elsif ($selection == 8) { Mashonisa->delete_client(); }
-        # elsif ($selection == 9) { Mashonisa->add_loan(); }
-        # elsif ($selection == 10) { Mashonisa->view_loans(); }
-        # elsif ($selection == 11) { Mashonisa->update_loan(); }
-        # elsif ($selection == 12) { Mashonisa->delete_loan(); }
-        # elsif ($selection == 13) { Mashonisa->add_loan_repayment(); }
-        # elsif ($selection == 14) { Mashonisa->view_loan_repayments(); }
-        # elsif ($selection == 15) { Mashonisa->update_loan_repayment(); }
-        # elsif ($selection == 16) { Mashonisa->delete_loan_repayment(); }
-        # elsif ($selection == 17) { Mashonisa->view_active_loans_grouped(); }
-        # elsif ($selection == 18) { Mashonisa->view_active_loans_client(); }
-        # elsif ($selection == 19) { Mashonisa->view_client_balance(); }
-        elsif ($selection == 20) {
-            say "Bye!\n";
-            last;
-        }
+            if ($selection == 1) { _create_agent( $MashonisaAgent ) }
+            elsif ($selection == 2) { _display_agents( $MashonisaAgent ) }
+            elsif ($selection == 3) { _update_agents( $MashonisaAgent ) }
+            elsif ($selection == 4) { _delete_agents( $MashonisaAgent ) }
+            # elsif ($selection == 5) { Mashonisa->add_client(); }
+            # elsif ($selection == 6) { Mashonisa->view_clients(); }
+            # elsif ($selection == 7) { Mashonisa->update_client(); }
+            # elsif ($selection == 8) { Mashonisa->delete_client(); }
+            # elsif ($selection == 9) { Mashonisa->add_loan(); }
+            # elsif ($selection == 10) { Mashonisa->view_loans(); }
+            # elsif ($selection == 11) { Mashonisa->update_loan(); }
+            # elsif ($selection == 12) { Mashonisa->delete_loan(); }
+            # elsif ($selection == 13) { Mashonisa->add_loan_repayment(); }
+            # elsif ($selection == 14) { Mashonisa->view_loan_repayments(); }
+            # elsif ($selection == 15) { Mashonisa->update_loan_repayment(); }
+            # elsif ($selection == 16) { Mashonisa->delete_loan_repayment(); }
+            # elsif ($selection == 17) { Mashonisa->view_active_loans_grouped(); }
+            # elsif ($selection == 18) { Mashonisa->view_active_loans_client(); }
+            # elsif ($selection == 19) { Mashonisa->view_client_balance(); }
+            elsif ($selection == 20) {
+                say "Bye!\n";
+                last;
+            }
             elsif ( $selection == 99 ) {
                 display_menu_options();
             }
-        else {
-                die "Invalid choice, try again!";
+            else {
+                die "Invalid choice, try again";
             }
         } catch ($error) {
-            say "$error";
-            return 0;
+            # TODO: figure out a way to print user friendly errors
+            # currently it prints out errors with the line numbers, great for debugging!
+            say $error;
         }
     }
 
+}
+
+sub _create_agent( $MashonisaAgent ) {
+
+    # TODO: handle when no name is given
+    print "\nEnter agent name: ";
+    chomp(my $name = <STDIN>);
+
+    # TODO: handle when no interest rate is given
+    print "\nEnter interest rate: ";
+    chomp(my $interest_rate = <STDIN>);
+    $interest_rate = _maybe_convert_to_decimal_number( $interest_rate );
+
+    my $AgentRS = $MashonisaAgent->add_agent($name, $interest_rate);
+
+    say "\nSuccessfully added Agent - '". $AgentRS->name ."' - with ". ($AgentRS->interest_rate->amount * 100) ."% interest rate.\n";
+}
+
+sub _display_agents( $MashonisaAgent ) {
+
+    print "\nEnter agent name or skip to view all agents: ";
+    chomp(my $name = <STDIN>);
+
+    my @agents = $MashonisaAgent->find_agents($name);
+    if ( ! @agents ) {
+        say "\nNo agents found" . ( $name ? " with name '$name'.\n" : ".\n" );
+        next;
+    }
+
+    foreach my $Agent ( @agents ) {
+        say "\nAgent name - ". $Agent->name;
+        say "Loan interest rate ". $Agent->interest_rate->amount * 100 ."%.";
+    }
+
+    print "\n";
+}
+
+# TODO: Refactor duplicate code in _display_agents, _update_agents, _delete_agents
+sub _update_agents( $MashonisaAgent ) {
+
+    print "\nEnter the name of the agent to UPDATE: ";
+    chomp(my $name = <STDIN>);
+
+    my @agents = $MashonisaAgent->find_agents($name);
+    if ( ! @agents ) {
+        say "\nNo agents found" . ( $name ? " with name '$name'.\n" : ".\n" );
+        next;
+    }
+
+    say "\nWe found ". scalar @agents ." agents";
+    my $count = 1;
+    foreach my $Agent ( @agents ) {
+        say "\nAgent ($count) - ". $Agent->name;
+        $count++;
+    }
+
+    print "\nEnter the number for the agent you want to UPDATE: ";
+    chomp(my $chosen_agent = <STDIN>);
+    # TODO: Handle invalid number choice
+    # also handle when entered a number greater than length of the list
+    my $ChosenAgent = $agents[$chosen_agent - 1];
+
+    print "\nEnter the NEW name for agent '". $ChosenAgent->name ."' OR skip: ";
+    chomp(my $new_name = <STDIN>);
+
+    print "\nChange the current interest rate of '"
+        . ( $ChosenAgent->interest_rate->amount * 100 ) ."%'"
+        . " for '". $ChosenAgent->name ."' OR skip: "
+    ;
+    chomp(my $new_interest_rate = <STDIN>);
+
+    $new_interest_rate = _maybe_convert_to_decimal_number($new_interest_rate);
+    $MashonisaAgent->update_agent(
+        $ChosenAgent->name,
+        $new_name,
+        $new_interest_rate
+    );
+
+    print "\n";
+    say "Successfully changed agent name from '". $ChosenAgent->name ."' to - '" . $new_name ."'.\n" if $new_name;
+    say "Successfully changed the interest_rate from '"
+        . ( $ChosenAgent->interest_rate->amount * 100 )
+        ."%' to - '" . ( $new_interest_rate * 100 )
+        ."%'.\n"
+    if $new_interest_rate;
+
+}
+
+sub _delete_agents( $MashonisaAgent ) {
+    print "\nEnter the name of the agent you want to DELETE: ";
+    chomp(my $name = <STDIN>);
+
+    my @agents = $MashonisaAgent->find_agents($name);
+    if ( ! @agents ) {
+        say "\nNo agents found" . ( $name ? " with name '$name'.\n" : ".\n" );
+        next;
+    }
+
+    say "\nWe found ". scalar @agents ." agents";
+    my $count = 1;
+    foreach my $Agent ( @agents ) {
+        say "\nAgent ($count) - ". $Agent->name;
+        $count++;
+    }
+
+    print "\nEnter the number for the agent you want to DELETE: ";
+    chomp(my $chosen_agent = <STDIN>);
+    my $ChosenAgent = $agents[$chosen_agent - 1];
+
+    $MashonisaAgent->delete_agent($ChosenAgent->name);
+    say "\nSuccessfully deleted agent '". $ChosenAgent->name ."' from database";
 }
 
 sub display_greeting_msg {
@@ -81,98 +194,12 @@ sub get_selected_option {
     return $selection;
 }
 
+sub _maybe_convert_to_decimal_number ($number) {
+    try {
+       $number && $number =~ m/(0\.)\d+/ ? $number : $number / 100
+    } catch( $error ) {
+        die $error;
+    };
+}
 
-# my $db_file = "mashonisa.db";
-# my $dbh = DBI->connect("dbi:SQLite:dbname=$db_file", "", "", { RaiseError => 1, AutoCommit => 1 });
-
-# sub main_menu {
-#     print "\nWelcome to Mashonisa!\n";
-#     print "1. Add Agent\n";
-#     print "2. View Agents\n";
-#     print "3. Update Agent\n";
-#     print "4. Delete Agent\n";
-#     print "5. Add Client\n";
-#     print "6. View Clients\n";
-#     print "7. Update Client\n";
-#     print "8. Delete Client\n";
-#     print "9. Add Loan\n";
-#     print "10. View Loans\n";
-#     print "11. Update Loan\n";
-#     print "12. Delete Loan\n";
-#     print "13. Add Loan Repayment\n";
-#     print "14. View Loan Repayments\n";
-#     print "15. Update Loan Repayment\n";
-#     print "16. Delete Loan Repayment\n";
-#     print "17. View Active Loans (Grouped by Month)\n";
-#     print "18. View Active Loans for a Client\n";
-#     print "19. View Client Balance\n";
-#     print "20. Exit\n";
-#     print "Choose an option: ";
-#     chomp(my $choice = <STDIN>);
-#     return $choice;
-# }
-
-# sub add_agent {
-#     print "Enter agent name: ";
-#     chomp(my $name = <STDIN>);
-#     my $sth = $dbh->prepare("INSERT INTO agent (name) VALUES (?)");
-#     $sth->execute($name);
-#     print "Agent added successfully!\n";
-# }
-
-# # sub view_agents {
-# #     my $sth = $dbh->prepare("SELECT * FROM agent");
-# #     $sth->execute();
-# #     while (my $row = $sth->fetchrow_hashref) {
-# #         print "ID: $row->{id}, Name: $row->{name}\n";
-# #     }
-# # }
-
-# # sub update_agent {
-# #     print "Enter agent ID to update: ";
-# #     chomp(my $id = <STDIN>);
-# #     print "Enter new name: ";
-# #     chomp(my $name = <STDIN>);
-# #     my $sth = $dbh->prepare("UPDATE agent SET name = ? WHERE id = ?");
-# #     $sth->execute($name, $id);
-# #     print "Agent updated successfully!\n";
-# # }
-
-# # sub delete_agent {
-# #     print "Enter agent ID to delete: ";
-# #     chomp(my $id = <STDIN>);
-# #     my $sth = $dbh->prepare("DELETE FROM agent WHERE id = ?");
-# #     $sth->execute($id);
-# #     print "Agent deleted successfully!\n";
-# # }
-
-
-# while (1) {
-#     my $choice = main_menu();
-#     if ($choice == 1) { add_agent(); }
-#     # elsif ($choice == 2) { view_agents(); }
-#     # elsif ($choice == 3) { update_agent(); }
-#     # elsif ($choice == 4) { delete_agent(); }
-#     # elsif ($choice == 5) { add_client(); }
-#     # elsif ($choice == 6) { view_clients(); }
-#     # elsif ($choice == 7) { update_client(); }
-#     # elsif ($choice == 8) { delete_client(); }
-#     # elsif ($choice == 9) { add_loan(); }
-#     # elsif ($choice == 10) { view_loans(); }
-#     # elsif ($choice == 11) { update_loan(); }
-#     # elsif ($choice == 12) { delete_loan(); }
-#     # elsif ($choice == 13) { add_loan_repayment(); }
-#     # elsif ($choice == 14) { view_loan_repayments(); }
-#     # elsif ($choice == 15) { update_loan_repayment(); }
-#     # elsif ($choice == 16) { delete_loan_repayment(); }
-#     # elsif ($choice == 17) { view_active_loans_grouped(); }
-#     # elsif ($choice == 18) { view_active_loans_client(); }
-#     # elsif ($choice == 19) { view_client_balance(); }
-#     elsif ($choice == 20) {
-#         print "Bye!\n";
-#         last;
-#     }
-#     else {
-#         print "Invalid choice, try again!\n";
-#     }
-# }
+1;
