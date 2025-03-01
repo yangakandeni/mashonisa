3eddcfa7fdb97e9048bba38a35f3cb3fda59fbdb
yangakandeni <kandeni.yanga@gmail.com>
2025-03-01 11:01:50 +0200

    fix: Keep menu short.
    Keeping the menu short by removing actions
    that do not need to implemented at the moment.

--- lib/Mashonisa.pm
+++ lib/Mashonisa.pm
@@ -12,28 +12,28 @@ use experimental qw/ signatures say /;
 
 sub main_menu ($self) {
     return qq/
-    1. Add Agent
-    2. View Agents
-    3. Update Agent
-    4. Delete Agent
-    5. Add Client
-    6. View Clients
-    7. Update Client
-    8. Delete Client
-    9. Add Loan
-    10. View Loans
-    11. Update Loan
-    12. Delete Loan
-    13. Add Loan Repayment
-    14. View Loan Repayments
-    15. Update Loan Repayment
-    16. Delete Loan Repayment
-    17. View Active Loans (Grouped by Month)
-    18. View Active Loans for a Client
-    19. View Client Balance
+    01. Add Agents
+    02. View Agents
+    03. Update Agents
+    04. Delete Agents
+    05. Add Loans
+    06. View Loans
+    07. Add Loan Repayments
+    08. View Loan Repayments
     20. Exit
     99. Go Back to Main Menu
     /;
+    # 11. Update Loan Repayment
+    # 12. Delete Loan Repayment
+    # 07. Update Loan
+    # 08. Delete Loan
+    # 5. Add Client
+    # 6. View Clients
+    # 7. Update Client
+    # 8. Delete Client
+    # 17. View Active Loans (Grouped by Month)
+    # 18. View Active Loans for a Client
+    # 19. View Client Balance
 }
 
 1;
